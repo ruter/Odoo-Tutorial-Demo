d1d1cf7e5306f064713c5a66ef271130a5fe7df9
Ruter <ruter.lv@gmail.com>
2018-03-28 17:23:46 +0800

    [ADD] TodoTask 模型

--- todo/models/models.py
+++ todo/models/models.py
@@ -2,14 +2,10 @@
 
 from odoo import models, fields, api
 
-# class todo(models.Model):
-#     _name = 'todo.todo'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class TodoTask(models.Model):
+     _name = 'todo.task'
+     _description = '待办事项'
+
+     name = fields.Char('描述', required=True)
+     is_done = fields.Boolean('已完成？')
