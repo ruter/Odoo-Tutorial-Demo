6152b632fd6e1d3691a8fa1480c6898b7e417a13
Ruter <ruter.lv@gmail.com>
2018-03-28 17:25:11 +0800

    [ADD] TodoTask 的入口菜单

--- todo/__manifest__.py
+++ todo/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+	'views/menus.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
