1dda615285a9c18c244656e45716c60de7887362
Ruter <ruter.lv@gmail.com>
2018-08-15 10:43:36 +0800

    [ADD] 添加紧急程度字段

--- todo/models/models.py
+++ todo/models/models.py
@@ -4,8 +4,13 @@ from odoo import models, fields, api
 
 
 class TodoTask(models.Model):
-     _name = 'todo.task'
-     _description = '待办事项'
+    _name = 'todo.task'
+    _description = '待办事项'
 
-     name = fields.Char('描述', required=True)
-     is_done = fields.Boolean('已完成？')
+    name = fields.Char('描述', required=True)
+    is_done = fields.Boolean('已完成？')
+    priority = fields.Selection([
+        ('todo', '待办'),
+        ('normal', '普通'),
+        ('urgency', '紧急')
+    ], default='todo', string='紧急程度')
