18c947928add03ee78f8614d7ecaa24cd776a520
ruter <ruter.lv@gmail.com>
2018-08-19 13:14:31 +0800

    [ADD] 新增分类

--- todo/models/models.py
+++ todo/models/models.py
@@ -3,6 +3,21 @@
 from odoo import models, fields, api
 
 
+class TodoCategory(models.Model):
+    _name = 'todo.category'
+    _description = '分类'
+
+    name = fields.Char(u'名称')
+    task_ids = fields.One2many('todo.task', 'category_id', string=u'代办事项')
+    count = fields.Integer(u'任务数量', compute='_compute_task_count')
+
+    @api.depends('task_ids')
+    @api.multi
+    def _compute_task_count(self):
+        for record in self:
+            record.count = len(record.task_ids)
+
+
 class TodoTask(models.Model):
     _name = 'todo.task'
     _description = '待办事项'
@@ -16,6 +31,7 @@ class TodoTask(models.Model):
     ], default='todo', string='紧急程度')
     deadline = fields.Datetime(u'截止时间')
     is_expired = fields.Boolean(u'已过期', compute='_compute_is_expired')
+    category_id = fields.Many2one('todo.category', string=u'分类')
 
     @api.depends('deadline')
     @api.multi
